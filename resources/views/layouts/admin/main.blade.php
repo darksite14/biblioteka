a931272f33a6b49adffd5f1e3db7b9f849b1233b
darksite14 <dawid@darksite.app>
2020-12-12 21:10:26 +0100

    oczyszczenie layoutu ze zbędnych funkcji

--- resources/views/layouts/admin/main.blade.php
+++ resources/views/layouts/admin/main.blade.php
@@ -1471,23 +1471,6 @@ License: You must have a valid license purchased only from themeforest(the above
 									<!--end::Dropdown-->
 								</div>
 								<!--end::Quick Actions-->
-								<!--begin::Cart-->
-								<div class="topbar-item">
-									<div class="btn btn-icon btn-clean btn-dropdown btn-lg mr-1" id="kt_quick_cart_toggle">
-										<span class="svg-icon svg-icon-xl svg-icon-primary">
-											<!--begin::Svg Icon | path:media/svg/icons/Shopping/Cart3.svg-->
-											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-												<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-													<rect x="0" y="0" width="24" height="24" />
-													<path d="M12,4.56204994 L7.76822128,9.6401844 C7.4146572,10.0644613 6.7840925,10.1217854 6.3598156,9.76822128 C5.9355387,9.4146572 5.87821464,8.7840925 6.23177872,8.3598156 L11.2317787,2.3598156 C11.6315738,1.88006147 12.3684262,1.88006147 12.7682213,2.3598156 L17.7682213,8.3598156 C18.1217854,8.7840925 18.0644613,9.4146572 17.6401844,9.76822128 C17.2159075,10.1217854 16.5853428,10.0644613 16.2317787,9.6401844 L12,4.56204994 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
-													<path d="M3.5,9 L20.5,9 C21.0522847,9 21.5,9.44771525 21.5,10 C21.5,10.132026 21.4738562,10.2627452 21.4230769,10.3846154 L17.7692308,19.1538462 C17.3034221,20.271787 16.2111026,21 15,21 L9,21 C7.78889745,21 6.6965779,20.271787 6.23076923,19.1538462 L2.57692308,10.3846154 C2.36450587,9.87481408 2.60558331,9.28934029 3.11538462,9.07692308 C3.23725479,9.02614384 3.36797398,9 3.5,9 Z M12,17 C13.1045695,17 14,16.1045695 14,15 C14,13.8954305 13.1045695,13 12,13 C10.8954305,13 10,13.8954305 10,15 C10,16.1045695 10.8954305,17 12,17 Z" fill="#000000" />
-												</g>
-											</svg>
-											<!--end::Svg Icon-->
-										</span>
-									</div>
-								</div>
-								<!--end::Cart-->
 								<!--begin::Quick panel-->
 								<div class="topbar-item">
 									<div class="btn btn-icon btn-clean btn-lg mr-1" id="kt_quick_panel_toggle">
@@ -1522,75 +1505,6 @@ License: You must have a valid license purchased only from themeforest(the above
 									</div>
 								</div>
 								<!--end::Chat-->
-								<!--begin::Languages-->
-								<div class="dropdown">
-									<!--begin::Toggle-->
-									<div class="topbar-item" data-toggle="dropdown" data-offset="10px,0px">
-										<div class="btn btn-icon btn-clean btn-dropdown btn-lg mr-1">
-											<img class="h-20px w-20px rounded-sm" src="media/svg/flags/226-united-states.svg" alt="" />
-										</div>
-									</div>
-									<!--end::Toggle-->
-									<!--begin::Dropdown-->
-									<div class="dropdown-menu p-0 m-0 dropdown-menu-anim-up dropdown-menu-sm dropdown-menu-right">
-										<!--begin::Nav-->
-										<ul class="navi navi-hover py-4">
-											<!--begin::Item-->
-											<li class="navi-item">
-												<a href="#" class="navi-link">
-													<span class="symbol symbol-20 mr-3">
-														<img src="media/svg/flags/226-united-states.svg" alt="" />
-													</span>
-													<span class="navi-text">English</span>
-												</a>
-											</li>
-											<!--end::Item-->
-											<!--begin::Item-->
-											<li class="navi-item active">
-												<a href="#" class="navi-link">
-													<span class="symbol symbol-20 mr-3">
-														<img src="media/svg/flags/128-spain.svg" alt="" />
-													</span>
-													<span class="navi-text">Spanish</span>
-												</a>
-											</li>
-											<!--end::Item-->
-											<!--begin::Item-->
-											<li class="navi-item">
-												<a href="#" class="navi-link">
-													<span class="symbol symbol-20 mr-3">
-														<img src="media/svg/flags/162-germany.svg" alt="" />
-													</span>
-													<span class="navi-text">German</span>
-												</a>
-											</li>
-											<!--end::Item-->
-											<!--begin::Item-->
-											<li class="navi-item">
-												<a href="#" class="navi-link">
-													<span class="symbol symbol-20 mr-3">
-														<img src="media/svg/flags/063-japan.svg" alt="" />
-													</span>
-													<span class="navi-text">Japanese</span>
-												</a>
-											</li>
-											<!--end::Item-->
-											<!--begin::Item-->
-											<li class="navi-item">
-												<a href="#" class="navi-link">
-													<span class="symbol symbol-20 mr-3">
-														<img src="media/svg/flags/195-france.svg" alt="" />
-													</span>
-													<span class="navi-text">French</span>
-												</a>
-											</li>
-											<!--end::Item-->
-										</ul>
-										<!--end::Nav-->
-									</div>
-									<!--end::Dropdown-->
-								</div>
-								<!--end::Languages-->
 								<!--begin::User-->
 								<div class="topbar-item">
 									<div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
@@ -1735,8 +1649,9 @@ License: You must have a valid license purchased only from themeforest(the above
 						<div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
 							<!--begin::Copyright-->
 							<div class="text-dark order-2 order-md-1">
-								<span class="text-muted font-weight-bold mr-2">2020©</span>
-								<a href="http://keenthemes.com/metronic" target="_blank" class="text-dark-75 text-hover-primary">Keenthemes</a>
+								<span class="text-muted font-weight-bold mr-2">2020 ©</span>
+								<span class="text-muted font-weight-bold mr-2">Made By</span>
+								<a href="https://darksite.app" target="_blank" class="text-dark-75 text-hover-primary">DarkSite</a>
 							</div>
 							<!--end::Copyright-->
 							<!--begin::Nav-->
@@ -2978,38 +2893,6 @@ License: You must have a valid license purchased only from themeforest(the above
 			</span>
 		</div>
 		<!--end::Scrolltop-->
-		<!--begin::Sticky Toolbar-->
-		<ul class="sticky-toolbar nav flex-column pl-2 pr-2 pt-3 pb-3 mt-4">
-			<!--begin::Item-->
-			<li class="nav-item mb-2" id="kt_demo_panel_toggle" data-toggle="tooltip" title="Check out more demos" data-placement="right">
-				<a class="btn btn-sm btn-icon btn-bg-light btn-icon-success btn-hover-success" href="#">
-					<i class="flaticon2-drop"></i>
-				</a>
-			</li>
-			<!--end::Item-->
-			<!--begin::Item-->
-			<li class="nav-item mb-2" data-toggle="tooltip" title="Layout Builder" data-placement="left">
-				<a class="btn btn-sm btn-icon btn-bg-light btn-icon-primary btn-hover-primary" href="https://preview.keenthemes.com/metronic/demo1/builder.html" target="_blank">
-					<i class="flaticon2-gear"></i>
-				</a>
-			</li>
-			<!--end::Item-->
-			<!--begin::Item-->
-			<li class="nav-item mb-2" data-toggle="tooltip" title="Documentation" data-placement="left">
-				<a class="btn btn-sm btn-icon btn-bg-light btn-icon-warning btn-hover-warning" href="https://keenthemes.com/metronic/?page=docs" target="_blank">
-					<i class="flaticon2-telegram-logo"></i>
-				</a>
-			</li>
-			<!--end::Item-->
-			<!--begin::Item-->
-			<li class="nav-item" id="kt_sticky_toolbar_chat_toggler" data-toggle="tooltip" title="Chat Example" data-placement="left">
-				<a class="btn btn-sm btn-icon btn-bg-light btn-icon-danger btn-hover-danger" href="#" data-toggle="modal" data-target="#kt_chat_modal">
-					<i class="flaticon2-chat-1"></i>
-				</a>
-			</li>
-			<!--end::Item-->
-		</ul>
-		<!--end::Sticky Toolbar-->
 		<!--begin::Demo Panel-->
 		<div id="kt_demo_panel" class="offcanvas offcanvas-right p-10">
 			<!--begin::Header-->
